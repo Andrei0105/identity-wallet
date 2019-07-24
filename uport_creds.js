793ea70374c89714afe89f068e3071fcaed28e02
Tudor-Andrei Vrabie <andrei0105@gmail.com>
2019-07-24 13:57:50 +0100

    Hiding the uport-wrapper using MutationObserver.

--- uport_creds.js
+++ uport_creds.js
@@ -13,11 +13,33 @@ function getCreds() {
     uport.requestDisclosure()
 
     uport.onResponse('disclosureReq').then(res => {
-      const did = res.payload.did
-      json = JSON.stringify(res.payload)
-      console.log(json)
-      document.querySelector('#msg').innerHTML = "Congratulations you are now <b>logged in</b>`.  Here is your DID identifier:  " + json
+        const did = res.payload.did
+        json = JSON.stringify(res.payload)
+        console.log(json)
+        document.querySelector('#msg').innerHTML = "Congratulations you are now <b>logged in</b>`.  Here is your DID identifier:  " + json
     })
 }
+
+// Hiding the uPort wrapper on insertion using MutationObserver
+// Select the node that will be observed for mutations
+var targetNode = document.getElementById('body');
+
+// Options for the observer (which mutations to observe)
+var config = { childList: true };
+
+// Callback function to execute when mutations are observed
+var callback = function (mutationsList, observer) {
+    for (var mutation of mutationsList) {
+        console.log('A child node has been added or removed.');
+        var uportWrapperDiv = document.getElementById('uport-wrapper');
+        uportWrapperDiv.style.display = 'none';
+    }
+};
+
+// Create an observer instance linked to the callback function
+var observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
 
 setTimeout(checkVariable, 3000);
