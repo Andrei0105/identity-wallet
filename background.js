6d26765b31021523ac980e172772587afed62a44
Tudor-Andrei Vrabie <andrei0105@gmail.com>
2019-07-27 20:09:44 +0100

    Minor cleanup.

--- background.js
+++ background.js
@@ -1,8 +1,4 @@
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({ color: '#3aa757' }, function () {
-    console.log("The color is green.");
-  });
-
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
@@ -20,5 +16,8 @@ chrome.runtime.onMessage.addListener(
       console.log('BG: received open popup from CS');
       chrome.storage.local.set({ 'tab_id': sender.tab.id });
       window.open("popup_fill.html", "extension_popup", "width=300,height=400,status=no,scrollbars=yes,resizable=no");
+    }
+    else {
+      console.log('BG: unrecognized message: ', request)
     }
   });
