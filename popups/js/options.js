fde852ea0bb153aeaf03a36c58d8059bc32c9ceb
Tudor-Andrei Vrabie <andrei0105@gmail.com>
2019-08-12 19:14:55 +0100

    Update options page UI.

--- popups/js/options.js
+++ popups/js/options.js
@@ -1,11 +1,20 @@
+function displayCurrentAgentEndpoint() {
+    chrome.storage.local.get(['aries_endpoint'], function (storageData) {
+        document.querySelector('#current_agent').innerHTML = "Current Agent endpoint is: " + storageData.aries_endpoint
+    });
+}
+
+
 function saveAgentEndpoint() {
     endpoint = $('#agent_endpoint').val()
     api_key = $('#agent_endpoint_api_key').val()
     console.log(endpoint, api_key)
     chrome.storage.local.set({ 'aries_endpoint': endpoint });
     chrome.storage.local.set({ 'aries_api_key': api_key });
+    displayCurrentAgentEndpoint();
 }
 
 $(document).ready(function () {
     $('#save_agent').click(saveAgentEndpoint);
+    displayCurrentAgentEndpoint();
 });
