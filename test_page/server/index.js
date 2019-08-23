7e7006feec8b9bad3032d1a4855e9b653d31e733
Tudor-Andrei Vrabie <andrei0105@gmail.com>
2019-08-23 14:40:43 +0100

    Add uPort server issue credential example.
    Note: Same flow as for login, since the same transport (qr code) is used the same way. No changes to extension code.

--- test_page/server/index.js
+++ test_page/server/index.js
@@ -13,7 +13,8 @@ let endpoint = ''
 const app = express();
 app.use(bodyParser.json({ type: '*/*' }))
 
-
+let responseReceived = false;
+let userCredential = null;
 //setup Credentials object with newly created application identity.
 const credentials = new Credentials({
     //test credentials
@@ -36,16 +37,58 @@ app.get('/', (req, res) => {
     })
 })
 
+app.get('/resp', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send(responseReceived);
+    if (responseReceived === true)
+        responseReceived = false;
+})
+
 app.post('/callback', (req, res) => {
     const jwt = req.body.access_token
     console.log(jwt);
+    responseReceived = true;
     credentials.authenticateDisclosureResponse(jwt).then(credentials => {
         console.log(credentials);
+        userCredential = credentials;
         // Validate the information and apply authorization logic
     }).catch(err => {
         console.log(err)
     })
 })
 
+app.post('/callback_issue', (req, res) => {
+    responseReceived = true;
+})
+
+app.get('/create_verification', (req, res) => {
+    if (userCredential) {
+        credentials.createVerification({
+            sub: userCredential.did,
+            exp: Math.floor(new Date().getTime() / 1000) + 30 * 24 * 60 * 60,
+            claim: { 'Identity': { 'Last Seen': `${new Date()}` } },
+            callbackUrl: endpoint + '/callback_issue'
+        }).then(attestation => {
+            console.log(`Encoded JWT sent to user: ${attestation}`)
+            console.log(`Decodeded JWT sent to user: ${JSON.stringify(decodeJWT(attestation))}`)
+            const uri = message.paramsToQueryString(message.messageToURI(attestation), { callback_type: 'post' })
+            const qr = transports.ui.getImageDataURI(uri)
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.send(qr)
+            userCredential = null;
+        })
+    }
+    else {
+        console.log('User not logged in!');
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.send('User not logged in');
+    }
+})
+
 // run the app server and tunneling service
-const server = app.listen(8088)
+const server = app.listen(8088, () => {
+    ngrok.connect({ addr: 8088 }).then(ngrokUrl => {
+        endpoint = ngrokUrl
+        console.log(`Login Service running, open at ${endpoint}`)
+    })
+})
