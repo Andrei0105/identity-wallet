90955375d482b49e462204843a3fc47620bfc5d6
Tudor-Andrei Vrabie <andrei0105@gmail.com>
2019-09-04 15:34:48 +0100

    Fix blockstack popup.
    Sending back auth response JWT still not working.

--- popups/js/popup_blockstack.js
+++ popups/js/popup_blockstack.js
@@ -1,13 +1,18 @@
-function useBlockstack() {
+async function useBlockstack() {
+    while (typeof window.blockstack === "undefined") {
+        await sleep(50);
+    };
+
     if (typeof window.blockstack !== "undefined") {
         chrome.storage.local.get(['auth_req'], function (data) {
             auth_req = data.auth_req;
-            document.getElementById('signin-button').addEventListener('click', function () {
-                blockstack.redirectToSignInWithAuthRequest(auth_req);
-                // console.log('R1', auth_req)
-            });
+            blockstack.redirectToSignInWithAuthRequest(auth_req);
         });
     }
 }
 
-setTimeout(useBlockstack, 500);
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+useBlockstack();
